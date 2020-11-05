57c19fdfcf47ebbde63efe76af7a4eedd0f2e56e
John Ferguson Smart <john.smart@wakaleo.com>
2020-11-05 19:51:00 +0000

    Forms exercises

--- src/test/java/serenitylabs/tutorials/seleniumeasy/selenium/WithWebdriverSupport.java
+++ src/test/java/serenitylabs/tutorials/seleniumeasy/selenium/WithWebdriverSupport.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -26,7 +27,6 @@ public abstract class WithWebdriverSupport {
     @Before
     public void openBrowser() {
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @After
